d0620c3c1708b82fa353a3d2d261b4f56591edb0
Chai Phonbopit <phonbopit@gmail.com>
2015-04-05 17:10:03 +0700

    add server.js'

--- server.js
+++ server.js
@@ -3,12 +3,31 @@ var path = require('path');
 
 var app = express();
 
+var adminRouter = express.Router();
+
 
 app.get('/', function(req, res) {
 
 	res.sendFile(path.join(__dirname + '/index.html'));
 
 });
+
+adminRouter.use(function(req, res, next) {
+
+	console.log(req.method + req.url);
+
+	// go to the next middleware
+	next();
+
+});
+
+adminRouter.get('/', function(req, res) {
+
+	// response
+
+});
+
+app.use('/admin', adminRouter);
 
 app.listen(5555, function() {
 
